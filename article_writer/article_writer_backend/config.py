b8cc523d2915a0400f447d14e9520601450f233b
aiwarrior-23 <himanshuit3036@gmail.com>
2024-05-29 17:26:06 +0530

    final_code

--- article_writer/article_writer_backend/config.py
+++ article_writer/article_writer_backend/config.py
@@ -1,12 +1,12 @@
 import os
 
-API_KEY = os.getenv('API_KEY', 'be40bf82a06e4c958cee025b20ffb836')
-AZURE_ENDPOINT = os.getenv('AZURE_ENDPOINT', 'https://rookie.openai.azure.com/')
-API_VERSION = os.getenv('API_VERSION', '2023-06-01-preview')
+API_KEY = os.getenv('API_KEY', '<your-key>')
+AZURE_ENDPOINT = os.getenv('AZURE_ENDPOINT', '<your-endpoint>')
+API_VERSION = os.getenv('API_VERSION', '<your-api-version>')
 LLM_CONFIG = {
     "api_key": API_KEY,
     "api_version": API_VERSION,
     "azure_endpoint": AZURE_ENDPOINT,
-    "model": "rookie",
+    "model": "<your-model>",
     "api_type": "azure"
 }
